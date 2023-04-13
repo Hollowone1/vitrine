b9e501f033e659e4146a43766c086794ddd96e10
Antoine Stoecklin <astoeck@hotmail.fr>
2023-04-13 21:43:40 +0200

    init website

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/assets/css/style.css.{html,ts}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
