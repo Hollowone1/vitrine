623de468466bce2d9e2268a3654dc88ceb1968b2
Antoine Stoecklin <astoeck@hotmail.fr>
2023-04-17 11:55:20 +0200

    started app routing module and index component

---
+++ src/app/pages/index/index.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+import { SeoService } from '../../services/seo.service';
+
+//import { OwlOptions } from 'ngx-owl-carousel-o';
+
+@Component({
+  selector: 'app-index',
+  templateUrl: './index.component.html'
+})
+export class IndexComponent{}
