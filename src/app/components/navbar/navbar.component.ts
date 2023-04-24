788fa98b07377ae5a4107636fe088c1fbe133a7e
Antoine Stoecklin <astoeck@hotmail.fr>
2023-04-24 11:49:27 +0200

    functionnal navbar

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -10,4 +10,23 @@ import { Component, OnInit } from '@angular/core';
     ngOnInit(): void {
         
     }
+
+    selectList() {
+			// Récupérer la liste et le bouton
+			var list = document.getElementById("open");
+			var button = document.getElementById("mainButton");
+			
+			// Vérifier si la liste est visible ou cachée
+			if (list.style.display === "none") {
+				// Afficher la liste
+				list.style.display = "block";
+				// Changer le texte du bouton
+				button.innerHTML = "Cacher la liste";
+			} else {
+				// Cacher la liste
+				list.style.display = "none";
+				// Changer le texte du bouton
+				button.innerHTML = "Afficher la liste";
+			}
+		}
   }
