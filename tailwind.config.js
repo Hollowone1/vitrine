781361cc9352578004437dd58d6eeb795fce0940
Antoine Stoecklin <astoeck@hotmail.fr>
2023-04-25 11:37:46 +0200

    new import and purge css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/app/pages/**/*.{html,js}',
-    './src/app/components/**/*.{html,js}',
+    './src/app/pages/**/*.{html,ts}',
+    './src/app/components/**/*.{html,ts}',
     './src/index.html',
     "./node_modules/tw-elements/dist/js/**/*.js"
   ],
+  mode:'jit',
   theme: {
     extend: {},
   },
